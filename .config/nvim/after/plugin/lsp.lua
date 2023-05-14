cc42248052188ed4e5497dabc594fed3ddf09479
Sean Dodson <dodsonsean@comcast.net>
2023-05-13 23:36:21 -0400

    add html language server

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,10 +1,10 @@
 local lsp = require('lsp-zero').preset("recommended")
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
 end)
 
-lsp.ensure_installed({'tsserver', 'eslint'})
+lsp.ensure_installed({ 'tsserver', 'eslint', 'html', 'lua_ls' })
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
