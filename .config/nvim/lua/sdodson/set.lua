c7f4d6c742b0139e8153cbed09811c3a492be6b9
Sean Dodson <dodsonsean@comcast.net>
2023-05-14 00:21:11 -0400

    turn off line wrapping

--- .config/nvim/lua/sdodson/set.lua
+++ .config/nvim/lua/sdodson/set.lua
@@ -8,6 +8,7 @@ vim.opt.softtabstop = 4;
 vim.opt.shiftwidth = 4;
 vim.opt.expandtab = true;
 vim.opt.smartindent = false;
+vim.opt.wrap = false;
 
 vim.opt.swapfile = false;
 vim.opt.backup = false;
