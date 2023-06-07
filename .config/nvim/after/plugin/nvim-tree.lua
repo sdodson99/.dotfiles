a1470e0cfea566bedf15bec4991c2e0c286e91fc
sdodson <sc.dodson4@gmail.com>
2023-06-07 00:07:13 -0400

    add nvim tree configuration

---
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,18 @@
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+vim.keymap.set("n", "<leader>b", vim.cmd.NvimTreeToggle)
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  }
+})
