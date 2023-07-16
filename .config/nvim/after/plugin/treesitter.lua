ca77875320dffa39b50b86dd6e2e6c8176acaaa5
sdodson <sc.dodson4@gmail.com>
2023-07-16 19:55:23 -0400

    fix weird new line tab indent

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,9 +1,12 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+    }
 }
